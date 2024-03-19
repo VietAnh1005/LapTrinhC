5110d3a219a04e8dc57e7354fc5d3e91f439c022
VietAnh1005 <120234031+VietAnh1005@users.noreply.github.com>
2024-03-19 14:49:01 +0700

    Add files via upload

---
+++ Bai1.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+void ktraBoi7HaiChuSo();
+
+int main(void)
+{
+	printf("Xuat ra man hinh:");
+	ktraBoi7HaiChuSo();
+	return 0;
+
+}
+
+void ktraBoi7HaiChuSo()
+{
+	for (int i = 10; i <= 99; i++)
+	{
+		if (i % 7 == 0)
+		{
+			printf("%d ", i);
+		}
+	}
+	
+}
---
+++ Bai2.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int ktraSCP(int num)
+{
+	int sqrtNum = sqrt((double)num);
+	return (sqrtNum / 1 * sqrtNum == num);
+}
+
+
+// Hàm đếm và in ra các số chính phương nhỏ hơn n
+void inSCP(int n)
+{
+	printf("Cac so chinh phuong nho hon %d la:\n", n);
+	int i = 1;
+	while (i * i < n)
+	{
+		if (ktraSCP(i))
+		{
+			printf("%d ", i * i);
+		}
+		i++;
+	}
+	printf("\n");
+}
+int main(void)
+{
+	int n;
+	printf("Nhap so: ");
+	scanf_s("%d", &n);
+
+	inSCP(n);
+
+	return 0;
+}
+
+
